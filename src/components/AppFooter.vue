dfc1099441c03e8ce7c3bcdfa27dbf74e1577fe6
ritweekraj2802 <67289913+ritweekraj2802@users.noreply.github.com>
2022-01-23 17:18:25 +0530

    MedBill

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="has-background-primary p-4">
+        <p class="label has-text-white">Made for CruzHacks 2022</p>
+    </footer>
+</template>
+
+<style>
+    footer {
+        margin-top: auto;
+    }
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="has-background-primary p-4">
+    <h1 class="title has-text-white">Medibill</h1>
+    
+  </header>
+
+</template>
